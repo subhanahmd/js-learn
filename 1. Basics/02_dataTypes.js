dee69ab5728c446a79c612ff35b09ca1419120d4
subhanahmd <subhan.ahmed199@gmail.com>
2025-04-30 13:36:05 +0500

    data types

---
+++ 1. Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) Node js mein not allowed
+
+console.log(3 + 3)
+
+// javascript Standerds ECMA
+
+let name = "Subhan" //String
+let age = 22 //int
+let isLoggedIn = false //bolean 
+
+// number ==> 2^53
+// bigint
+// string => ""
+// boolean => True/False
+// null => Standalone value
+// undefined
+// Symbol => Unique
+
+// Object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
