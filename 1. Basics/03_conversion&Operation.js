1c9bfe3dbf73bd8b5ffd53f110bb324f9ad82558
subhanahmd <subhan.ahmed199@gmail.com>
2025-04-30 15:28:43 +0500

    operations

--- 1. Basics/03_conversion&Operation.js
+++ 1. Basics/03_conversion&Operation.js
@@ -34,4 +34,28 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 //33 => "33"
+
+// ****************** Operations ********************* //
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2**3); // power
+
+let str1 = "hello"
+let str2 = " Subhan"
+let str3 = str1 + str2
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122 (Converts whole to string)
+console.log(1 + 2 + "2"); // 32
+
+// if string firts whole will be converted to string
+
+console.log(+true); //=> 1
+console.log(+""); //=> 0
  
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 //donot do this
